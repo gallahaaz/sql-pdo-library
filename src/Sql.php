5b160bfa13eeedf2d4c7e585a8be7fef1d431e5d
gallahaaz <kevin.gallahaaz@gmail.com>
2017-05-24 23:00:03 -0300

    terminadas funções de insert, update, delete e call

--- src/Sql.php
+++ src/Sql.php
@@ -1,55 +1,199 @@
-<?php 
+<?php
 
-Namespace Gallahaaz\SqlPDOLibrary
+Namespace Gallahaaz\SqlPDOLibrary;
 
-class Sql extends PDO 
-{
+use PDO;
 
-	private $sql;
+class Sql extends PDO {
 
-	public function __construct(){
-		$this->sql = new PDO("mysql:host=" . DBHOST . ";dbname=" . DBSCHEMA, DBUSER, DBPASSWORD);
-	}
+    private $sql;
+    private $sqlCommands = [
+        'NOW()',
+        'CURDATE()',
+        'CURTIME()'
+    ];
 
-	private function setParam( $statment, $key, $value ){
-		$statment->bindParam( $key, $value );
-	}
+    public function __construct() {
+        $host = DBHOST;
+        $schema = DBSCHEMA;
+        $login = DBLOGIN;
+        $password = DBPASSWORD;
+        $this->sql = new PDO("mysql:host=$host;dbname=$schema", $login, $password );
+    }
 
-	private function setParams( $statment, $parameters = [] ){
-		foreach( $parameters as $key => $value ){
-			$this->setParam( $statment, $key, $value );
-		}
-	}
+    private function setParam($statment, $key, $value) {
+        $statment->bindParam($key, $value);
+    }
 
-	public function query( $raw, $params = [] ){
-		$stmt = $this->sql->prepare($raw);
-		$this->setParams( $stmt, $params );
-		$stmt->execute();
-		return $stmt;
-	}
+    private function setParams($statment, $parameters = []) {
+        var_dump($parameters);
+        foreach ( $parameters as $key => $value ) {
+            echo $key . " : " . $value;
+            $this->setParam($statment, $key, $value);
+        }
+    }
 
-	public function fetchAssoc( $rawQuery, $params = [] ):array{
-		$stmt = $this->query( $rawQuery, $params );
-		$stmt->fetchAll( PDO::FETCH_ASSOC );
-	}
+    public function query( $raw, $params = [] ) {
+        $stmt = $this->sql->prepare( $raw );
+        $this->setParams( $stmt, $params );
+        $data = $stmt->execute();
+        return $stmt;
+    }
 
-	public function select( $fields, $table, $searchFields = null, $options=null ){
-	}
+    public function fetchAssoc( $rawQuery, $params = [] ){
+        $stmt = $this->query( $rawQuery, $params );
+        return $stmt->fetchAll( PDO::FETCH_ASSOC );
+    }
 
-	public function insert( $table, $columns, $values ){
+    public function singleFetchAssoc( $rawQuery, $params = [] ){
+        $rawQuery .= " LIMIT 1";
+        $stmt = $this->query( $rawQuery, $params );
+        $data = $stmt->fetchAll( PDO::FETCH_ASSOC );
+        return $data[0];
+    }
+    
+    public function select( $fields, $table, $parameters = null, $options = null ) {
+        $cmd = "SELECT "
+            . $this->concatArray( $fields )
+            . " FROM "
+            . $table;
+        if( isset( $parameters ) ){
+            $cmd .= " WHERE " . $this->concatAndParams( $parameters );
+        }
+        if( isset($options) ){
+            $cmd .= $options;
+        }
+        return $this->fetchAssoc( $cmd, $parameters );
+    }
 
-	}
+    public function insert( $table, $columns, $values ) {
+        $cmd = "INSERT INTO "
+            . $table
+            . " (" . $this->concatArray($columns) . ") "
+            . " VALUES "
+            . " (" . $this->concatParams($columns) . ");";
+        $keys = [];
+        foreach( $columns as $key ){
+            array_push( $keys, ':' . $key );
+        }
+        $parameters = array_combine( $keys, $values );
+        return $this->query( $cmd, $parameters );
+    }
+    
+    public function update( $table, $set, $where ) {
+        $cmd = "UPDATE "
+            . $table
+            . " SET "
+            . $this->concatSetParams( $set )
+            . " WHERE "
+            . $this->concatAndParams( $where );
+        $parameters = [];
+        $combined = array_merge($set, $where);
+        foreach( $combined as $key => $value ){
+            $parameters[":" . $key] = $value;
+        }
+        $this->query( $cmd, $parameters );
+    }
 
-	public function update( $table, $set, $where ){
+    public function delete( $table, $where ) {
+        $cmd = "DELETE FROM " . $table
+            . " WHERE " . $this->concatAndParams( $where );
+        $parameters = [];
+        foreach( $where as $key => $value ){
+            $parameters[":" . $key] = $value;
+        }
+        $this->query( $cmd, $parameters );
+    }
 
-	}
-
-	public function delete( $table, $where ){
-
-	}
-
-	public function call( $procedure, $arguments, $single = false ){
-		return $this->fetchAssoc();
-	}
-
+    public function call( $procedure, $arguments, $single = false ) {
+        $cmd = "CALL " . $procedure
+            . "(" . $this->concatArrayValues( $arguments ) . ")";
+        $this->fetchAssoc($cmd);
+    }
+    
+    public function concatArray( $array ){
+        $string = '';
+        $c = 0;
+        $last = count( $array );
+        foreach( $array as $key ){
+            if( ( $c >= 1 ) &&( $c<$last ) ){
+                $string .= ',' ;
+                $string .= $key;
+            }else{
+                $string .= $key;
+            }
+            $c++;
+        }
+        return $string;
+    }
+    
+    public function concatParams( $array ){
+        $string = '';
+        $c = 0;
+        $last = count( $array );
+        foreach( $array as $key => $value ){ 
+            if( ( $c >= 1 ) &&( $c<$last ) ){
+                $string .= ",";
+            }
+            $string .= ' :' . $value;
+            $c++;
+        }
+        return $string;
+    }
+    
+    public function concatAndParams( $array ){
+        $string = '';
+        $c = 0;
+        $last = count( $array );
+        foreach( $array as $key => $value ){
+            if( ( $c >= 1 ) &&( $c<$last ) ){
+                $string .= " AND ";
+            }
+            $string .= $key . ' = :' . $key;
+            $c++;
+        }
+        return $string;
+    }
+    
+    public function concatSetParams( $array ){
+        $string = '';
+        $c = 0;
+        $last = count( $array );
+        foreach( $array as $key => $value ){
+            if( ( $c >= 1 ) &&( $c<$last ) ){
+                $string .= ", ";
+            }
+            $string .= $key . ' = :' . $key;
+            $c++;
+        }
+        return $string;
+    }
+    
+    public function concatArrayValues( $array ){
+        $string = '';
+        $c = 0;
+        $last = count( $array );
+        foreach( $array as $key ){
+            if( ( $c >= 1 ) &&( $c<$last ) ){
+                if( is_integer($key) || is_float($key) ){
+                    $string .= ", " . str_replace( ',', '.', $key);
+                }else{
+                    if( in_array( $key, $this->sqlCommands ) ){
+                        $string .= ", " . $key . " ";
+                    }else{
+                        $string .= ", '" . urlencode($key) . "' ";
+                    }
+                }
+            }else{
+                if( is_integer($key) || is_float($key) ){
+                    $string .= $key ;
+                }else{
+                    $string .= " '" . $key . "' ";
+                }
+            }
+            $c++;
+        }
+        return $string;
+    }
+    
 }
